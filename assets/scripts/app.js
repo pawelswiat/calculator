43e36337f65099a1228d4d6228b9c869c96c76fb
pawelswiat <pawelswiat89@gmail.com>
2021-06-15 23:59:32 +0200

    working on result

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!');
+let currentResult = 0;
+let calculationDescription = '(0 + 10) * 3 / 2 - 1'
+
+currentResult = '(' + currentResult + ' + 10) * 3 / 2 - 1';
+
+
+
+outputResult(currentResult, calculationDescription);
