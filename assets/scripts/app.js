12d025d29915ab8dc00d7de23103f217312eaeed
pawelswiat <pawelswiat89@gmail.com>
2021-06-17 00:11:03 +0200

    add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,14 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+currentResult = add(1,2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
+
+
+function add(number1, number2) {
+    const result = number1 + number2;
+    return result
+} 
